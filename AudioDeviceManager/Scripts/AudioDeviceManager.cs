806c532007a5fb19f29aee57a0ac8a47b71b5c08
Gil Moutinho <gil.moutinho@exaud.com>
2021-09-02 16:25:25 +0100

    Added scripts

---
+++ AudioDeviceManager/Scripts/AudioDeviceManager.cs
@@ -0,0 +1,177 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+/// <summary>
+/// Class with methods to show and switch audio devices.
+/// </summary>
+public static class AudioDeviceManager
+{
+    private const string AudioDevicesFile = "audioDevices.txt";
+
+    /// <summary>
+    /// Gets the current default audio output device.
+    /// </summary>
+    /// <param name="deviceRole">Role of the device (optional, defaults to Console)</param>
+    /// <returns>The current audio output device</returns>
+    public static Device GetCurrentDevice(DeviceRole deviceRole = DeviceRole.Console)
+    {
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+            case RuntimePlatform.WindowsEditor:
+                // TODO: Uncomment after the method is fixed.
+                //return GetCurrentDeviceWindows(deviceRole);
+                Debug.LogError("GetCurrentDevice is not working in Windows.");
+                return null;
+            case RuntimePlatform.OSXPlayer:
+            case RuntimePlatform.OSXEditor:
+                return GetCurrentDeviceMacOS();
+            default:
+                Debug.LogError("AudioDeviceManager is only available for Windows and macOS!");
+                return null;
+        }
+    }
+
+    /// <summary>
+    /// Gets the current default audio output device in Windows.
+    /// FIXME: This method is not working.
+    /// </summary>
+    /// <param name="deviceRole">Role of the device</param>
+    /// <returns>The current audio output device</returns>
+    private static Device GetCurrentDeviceWindows(DeviceRole deviceRole)
+    {
+        const int DefaultConsoleDeviceToken = 5;
+        int defaultDeviceToken = DefaultConsoleDeviceToken + ((int)deviceRole);
+        string arguments = "/scomma " + AudioDevicesFile + " && " +
+            "for /f \"tokens=1,4," + defaultDeviceToken + " delims=,\" " +
+            "%a in ('type " + AudioDevicesFile + " ^| find \"Render\"') " +
+            "do @echo %a (%b),%c";
+        Device currentDevice = null;
+        AudioDeviceManagerCommands.ExecuteCommand(arguments, (output) =>
+        {
+            foreach (string deviceString in output)
+            {
+                string[] splitDeviceString = deviceString.Split(',');
+                if (splitDeviceString[1] == "Render")
+                {
+                    currentDevice = new Device(splitDeviceString[0]);
+                    break;
+                }
+            }
+        });
+        return currentDevice;
+    }
+
+    /// <summary>
+    /// Gets the current default audio output device in macOS.
+    /// </summary>
+    /// <returns>The current audio output device</returns>
+    private static Device GetCurrentDeviceMacOS()
+    {
+        string arguments = "-c -t output";
+        Device currentDevice = null;
+        AudioDeviceManagerCommands.ExecuteCommand(arguments, (output) =>
+        {
+            if (output.Count > 0)
+            {
+                currentDevice = new Device(output[0]);
+            }
+        });
+        return currentDevice;
+    }
+
+    /// <summary>
+    /// Gets an array with all the available audio output devices.
+    /// </summary>
+    /// <returns>Array with the current audio output devices</returns>
+    public static Device[] GetAllDevices()
+    {
+        string arguments;
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+            case RuntimePlatform.WindowsEditor:
+                // FIXME: This is not working.
+                //arguments = "/scomma " + AudioDevicesFile + " && " +
+                //    "for /f \"tokens=1,4 delims=,\" " +
+                //    "%a in ('type " + AudioDevicesFile + " ^| find \"Render\"') " +
+                //    "do @echo %a (%b)";
+                //break;
+                Debug.LogError("GetAllDevices is not working in Windows.");
+                return null;
+            case RuntimePlatform.OSXPlayer:
+            case RuntimePlatform.OSXEditor:
+                arguments = "-a -t output";
+                break;
+            default:
+                Debug.LogError("AudioDeviceManager is only available for Windows and macOS!");
+                return null;
+        }
+
+        Device[] devices = new Device[0];
+        AudioDeviceManagerCommands.ExecuteCommand(arguments, (output) =>
+        {
+            devices = output.Select(deviceName => new Device(deviceName)).ToArray();
+        });
+        return devices;
+    }
+
+    /// <summary>
+    /// Asynchronously changes the current default audio output device.
+    /// </summary>
+    /// <param name="deviceName">Name of the device to set as default</param>
+    /// <param name="deviceRole">Role of the device (optional, defaults to Console)</param>
+    public static void SetDefaultDevice(string deviceName, DeviceRole deviceRole = DeviceRole.Console)
+    {
+        string arguments;
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+            case RuntimePlatform.WindowsEditor:
+                deviceName = deviceName.Replace("(default)", string.Empty);
+                GroupCollection regexGroups = Regex.Match(deviceName, @"([^)(]+)\((.+)\)").Groups;
+                string name = regexGroups[1].Value.Trim();
+                string manufactor = regexGroups[2].Value.Trim();
+                string commandLineFriendlyID = manufactor + @"\Device\" + name + @"\Render";
+                arguments = "/SetDefault \"" + commandLineFriendlyID + "\" " + (int)deviceRole;
+                break;
+            case RuntimePlatform.OSXPlayer:
+            case RuntimePlatform.OSXEditor:
+                arguments = "-t output -s '" + deviceName + "'";
+                break;
+            default:
+                Debug.LogError("AudioDeviceManager is only available for Windows and macOS!");
+                return;
+        }
+
+        AudioDeviceManagerCommands.ExecuteAsyncCommand(arguments);
+    }
+
+    /// <summary>
+    /// Class with audio device information.
+    /// </summary>
+    public class Device
+    {
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name">Name of the device</param>
+        public Device(string name)
+        {
+            Name = name;
+        }
+    }
+
+    /// <summary>
+    /// Enum with the different types of audio device roles.
+    /// </summary>
+    public enum DeviceRole
+    {
+        Console,
+        Multimedia,
+        Communications
+    }
+}
---
+++ AudioDeviceManager/Scripts/AudioDeviceManagerCommands.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+/// <summary>
+/// Class with the methods to run scripts in the command line.
+/// </summary>
+public static class AudioDeviceManagerCommands
+{
+    private static string BasePath => Application.streamingAssetsPath + "/AudioDeviceManager";
+
+    private static string ExecutablePath
+    {
+        get
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.WindowsEditor:
+                    // Check if the build is 64bit or 32bit.
+                    return IntPtr.Size == 8
+                        ? BasePath + "/win64/SoundVolumeView.exe"
+                        : BasePath + "/win32/SoundVolumeView.exe";
+                case RuntimePlatform.OSXPlayer:
+                case RuntimePlatform.OSXEditor:
+                    return BasePath + "/macOS/SwitchAudioSource";
+                default:
+                    UnityEngine.Debug.LogError("AudioDeviceManager is only available for Windows and macOS!");
+                    return null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Executes a command in the command line.
+    /// </summary>
+    /// <param name="arguments">Command arguments</param>
+    /// <param name="standardOutputAction">Action to deal with the command output (optional)</param>
+    public static void ExecuteCommand(string arguments, Action<List<string>> standardOutputAction = null)
+    {
+        Command(ExecutablePath, arguments, standardOutputAction);
+    }
+
+    /// <summary>
+    /// Creates a thread to execute a command in the command line.
+    /// </summary>
+    /// <param name="arguments">Command arguments</param>
+    /// <param name="standardOutputAction">Action to deal with the command output (optional)</param>
+    public static void ExecuteAsyncCommand(string arguments, Action<List<string>> standardOutputAction = null)
+    {
+        string executablePath = ExecutablePath;
+        Thread thread = new Thread(delegate () { Command(executablePath, arguments, standardOutputAction); });
+        thread.Start();
+    }
+
+    /// <summary>
+    /// Executes a command in the command line.
+    /// </summary>
+    /// <param name="executablePath">Path of the executable</param>
+    /// <param name="arguments">Command arguments</param>
+    /// <param name="standardOutputAction">Action to deal with the command output (nullable)</param>
+    private static void Command(string executablePath, string arguments, Action<List<string>> standardOutputAction)
+    {
+        ProcessStartInfo processInfo = new ProcessStartInfo
+        {
+            FileName = executablePath,
+            Arguments = arguments,
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardOutput = standardOutputAction != null
+        };
+
+        Process process = Process.Start(processInfo);
+
+        if (standardOutputAction != null)
+        {
+            List<string> lines = new List<string>();
+            while (process.StandardOutput.EndOfStream == false)
+            {
+                lines.Add(process.StandardOutput.ReadLine());
+            }
+            standardOutputAction(lines);
+        }
+
+        process.WaitForExit();
+        process.Close();
+    }
+}
